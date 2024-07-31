bdb5e7cc472645aed40f926135201b45245970a2
Mono-eyeddev <148053486+Mono-eyeddev@users.noreply.github.com>
2024-08-01 00:38:04 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// Creating  variables of different data types
+let greeting = "Welcome to JavaScript!"; // string
+let age = 30; // number
+let isStudent = false; // boolean
+let emptyValue = null; // null
+let notDefined; // undefined
+let person = { firstName: "Daniel", lastName: "Moki" }; // object
+let uniqueId = Symbol('id'); // symbol
