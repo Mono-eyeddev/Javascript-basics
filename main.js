69acb2f855a2b1d145505af2f252e8f3dc53954b
sajaad iqbal <sajaadiqbalkarim7@gmail.com>
2024-08-01 00:52:28 +0300

    printing variables to console

--- main.js
+++ main.js
@@ -6,3 +6,11 @@ let emptyValue = null; // null
 let notDefined; // undefined
 let person = { firstName: "Daniel", lastName: "Moki" }; // object
 let uniqueId = Symbol('id'); // symbol
+//  Printing the variables to the console
+console.log("Greeting:", greeting);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+console.log("Empty Value:", emptyValue);
+console.log("Not Defined:", notDefined);
+console.log("Person Object:", person);
+console.log("Unique ID Symbol:", uniqueId);
